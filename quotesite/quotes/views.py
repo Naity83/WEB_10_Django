5886400acc4d651bca250f0b67edc150b6ea7d9b
Nataliya Khomenko <NataliaHomenko83@gmail.com>
2024-05-01 09:39:05 +0300

    one_task

--- quotesite/quotes/views.py
+++ quotesite/quotes/views.py
@@ -11,6 +11,7 @@ def main(request, page=1):
     per_page = 10
     paginator = Paginator(list(quotes), per_page)
     quotes_on_page = paginator.page(page)
+    print(list(quotes))
     return render(request, 'quotes/index.html', context={'quotes':quotes_on_page})
 
 def author_about(request, author_id):
@@ -18,4 +19,12 @@ def author_about(request, author_id):
     author = db.authors.find_one({'_id': ObjectId(author_id)})  # Використання ObjectId(author_id)
     return render(request, 'quotes/author.html', context={'author': author})
 
+def tag_page(request, tag_name):
+    tag = Tag.objects.get(name=tag_name)
+    quotes_with_tag = Quote.objects.filter(tags=tag)
+    return render(request, 'quotes/tag.html', {'quotes_with_tag': quotes_with_tag, 'tag': tag})
 
+
+def author_for_tag(request, author_id):
+    author = Author.objects.get(id=author_id)
+    return render(request, 'quotes/author_for_tag.html', context={'author': author})
